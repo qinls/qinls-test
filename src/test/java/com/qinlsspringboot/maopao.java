ec6b125e69ce3e1679866df169fe788bb06fbba5
qinls <qinlongs@foxmail.com>
2018-12-05 00:52:28 +0800

    加日志注解

--- src/test/java/com/qinlsspringboot/maopao.java
+++ src/test/java/com/qinlsspringboot/maopao.java
@@ -1,5 +1,6 @@
 package com.qinlsspringboot;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,6 +14,7 @@ import java.util.Arrays;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
 public class maopao {
 
     @Test
